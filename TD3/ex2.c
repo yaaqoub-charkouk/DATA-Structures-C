9d2b2fd2badbc042c0437dd1538f6f06b2182e10
yaaqoub-charkouk <yaaqoubcourses@gmail.com>
2025-05-21 12:20:12 +0100

    tree_utils

--- TD3/ex2.c
+++ TD3/ex2.c
@@ -18,11 +18,82 @@ ArbreB  *creer_arbre(int valeur, ArbreB *G, ArbreB *D)
 	return (node);
 }
 
-void    afficherA(ArbreB *arbre)
+void    afficher_prefixe(ArbreB *arbre) // Algorithm prefixe
 {
 	if (!arbre)
+		printf(" . ") ;
+	else
+	{
+		printf(" %d ", arbre->valeur);
+		afficherA(arbre->gauche);
+		afficherA(arbre->droite);
+	}
+}
+
+void	afficher_postfixe(ArbreB *arbre)
+{
+	if (!arbre)
+		printf(" . ") ;
+	else
+	{
+		afficherA(arbre->gauche);
+		afficherA(arbre->droite);
+		printf(" %d ", arbre->valeur);
+	}
+}
+
+void	afficher_infixe(ArbreB *arbre)
+{
+	if (!arbre)
+		printf(" . ") ;
+	else
+	{
+		afficherA(arbre->gauche);
+		printf(" %d ", arbre->valeur);
+		afficherA(arbre->droite);
+	}
+}
+
+int	nbr_noeuds(ArbreB *arbre)
+{
+	if (!arbre)
+		return (0);
+	return (1 + nbr_noeuds(arbre->gauche) + nbr_noeuds(arbre->droite));
+}
+
+int	max(int	x, int	y)
+{
+	if (x > y) return (x);
+	else	return (y);
+}
+
+int	hauteur(ArbreB *arbre)
+{
+	if (!arbre)
+		return (0);
+	return (1 + max(hauteur(arbre->gauche), hauteur(arbre->droite)));
+}
+
+int	recherche(ArbreB *arbre, int e)
+{
+	if (!arbre)
+		return (0);
+	if (e == arbre->valeur)
+		return (1);
+	else
+		return (recherche(arbre->gauche, e) + recherche(arbre->droite, e));
+}
+
+void	free_tree(ArbreB *a)
+{
+	if (!a)
 		return ;
-	afficherA(arbre->gauche);
-	afficherA(arbre->droite);
-	printf("%d", arbre->valeur);
+	else
+	{
+		free_tree(a->gauche);
+		free_tree(a->droite);
+		free(a);
+		a = NULL;
+		return ;
+	}
 }
